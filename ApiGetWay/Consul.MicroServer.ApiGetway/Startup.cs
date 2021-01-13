fa7e85f35720fcbf4bfe2876fe2447840c9171df
zhangfaguo <zhangfaguo98@163.com>
2021-01-13 15:31:08 +0800

    2

--- ApiGetWay/Consul.MicroServer.ApiGetway/Startup.cs
+++ ApiGetWay/Consul.MicroServer.ApiGetway/Startup.cs
@@ -1,18 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Consul.MicroServer.ApiGetway
 {
@@ -29,10 +21,6 @@ namespace Consul.MicroServer.ApiGetway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot().AddConsul();
-          //  services.AddControllers();
-
-        
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
